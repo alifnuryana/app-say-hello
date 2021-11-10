eb1b4b87a764b3c3396a0335efcb79136ba27f08
alifnuryana <alifnuryana0@gmail.com>
2021-11-11 05:46:14 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alifnuryana/app-say-hello
+
+go 1.17
+
+require github.com/alifnuryana/go-say-hello v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/alifnuryana/go-say-hello"
+)
+
+func main() {
+	fmt.Println(SayHello())
+}
